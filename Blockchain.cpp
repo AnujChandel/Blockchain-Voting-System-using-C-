f52236c84266ba974b32eeedd1d8589bb6f2107a
REX <103368827+AnujChandel@users.noreply.github.com>
2023-05-11 13:02:09 +0530

    Update Blockchain.cpp
    updated the code

--- Blockchain.cpp
+++ Blockchain.cpp
@@ -1,169 +1,179 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
+#include <vector>
+#include <algorithm>
 #include "picosha2.h"
 
 using namespace std;
 using namespace picosha2;
 
-class block {
+class Block {
 public:
     string data;
-    string prev_hash;
+    string prevHash;
     string hash;
-    block* next;
+    Block* next;
+
+    Block(const string& data, const string& prevHash) : data(data), prevHash(prevHash), next(nullptr) {
+        calculateHash();
+    }
+
+    void calculateHash() {
+        stringstream concat;
+        concat << prevHash << data;
+        hash = hash256_hex_string(concat.str());
+    }
 };
 
-void makefile(block*);
-void count(block*);
+class Blockchain {
+private:
+    Block* head;
 
-string calc_hash(string prev_hash, string data) {
+public:
+    Blockchain() : head(nullptr) {}
 
-    string p_hash = prev_hash;
-    string m_data = data;
-    string new_hash;
-    stringstream concat;
-    concat << p_hash << m_data;
-    new_hash = hash256_hex_string(concat.str());
-    return new_hash;
-}
-
-void genesis_block(block** headref) {
-    if (*headref == NULL) {
-        block* last = *headref;
-        block* new_block = new block();
-        new_block->next = NULL;
-        new_block->data = "0";
-        new_block->prev_hash = "0";
-        new_block->hash = calc_hash(new_block->prev_hash, new_block->data);
-        *headref = new_block;
-        makefile(new_block);
-        return;
-    }
-}
-
-void makefile(block* headref) {
-    block* last = headref;
-    while (last->next != NULL) {
-        last = last->next;
-    }
-    fstream hashfile;
-    hashfile.open("lasthash.txt", ios::out);
-    if (hashfile.is_open()) {
-        hashfile << last->hash;
-    }
-}
-
-void new_block(block** headref, string new_data) {
-    if (*headref != NULL) {
-        block* last = *headref;
-        block* new_block = new block();
-        new_block->data = new_data;
-        new_block->next = NULL;
-        while (last->next != NULL) {
-            last = last->next;
+    void addGenesisBlock() {
+        if (head == nullptr) {
+            head = new Block("0", "0");
+            saveToFile(head->hash);
         }
-        new_block->prev_hash = last->hash;
-        new_block->hash = calc_hash(new_block->prev_hash, new_block->data);
-        last->next = new_block;
-        makefile(new_block);
     }
-    return;
-}
 
-string getlasthash(block* headref) {
-    string nm_hash;
-    if (headref->next == NULL) {
-        string temp1 = "f1534392279bddbf9d43dde8701cb5be14b82f76ec6607bf8d6ad557f60f304e";
-        return temp1;
-    }
-    while (headref->next != NULL) {
-        headref = headref->next;
-        nm_hash = calc_hash(headref->prev_hash, headref->data);
-    }
-    return nm_hash;
-}
-
-bool verify(block* headref) {
-    string lasthash = getlasthash(headref);
-    string filehash;
-    string filf;
-    ifstream hashfil;
-    hashfil.open("lasthash.txt", ios::in);
-    if (hashfil.is_open()) {
-        while (1) {
-            hashfil >> filf;
-            if (hashfil.eof()) {
-                break;
+    void addBlock(const string& data) {
+        if (head != nullptr) {
+            Block* last = head;
+            while (last->next != nullptr) {
+                last = last->next;
             }
+            Block* newBlock = new Block(data, last->hash);
+            last->next = newBlock;
+            saveToFile(newBlock->hash);
         }
-        hashfil.close();
-        return filf == lasthash;
     }
-}
 
-void print(block* headref) {
-    headref = headref->next;
-    while (headref != NULL) {
-        cout << headref->data << "->";
-        headref = headref->next;
-    }
-    cout << "END\n";
-}
-
-void tocheckwinner(block* headref){
-    int count1 = 0, count2 = 0, count3 = 0;
-    while(headref != NULL){
-        if(headref->data == "1")
-            count1++;
-        if(headref->data == "2")
-            count2++;
-        if(headref->data == "3")
-            count3++;
-        headref = headref->next;
-    }
-    if(count1 > count2 && count1 > count3){
-        cout << "Winner is: RAHUL SINGH" << endl;
-        return;
-    }
-    if(count2 > count1 && count2 > count3){
-        cout << "Winner is: KOMAL GUPTA" << endl;
-        return;
-    }
-    if(count3 > count1 && count3 > count2){
-        cout << "Winner is: ABHISHEK TOMAR" << endl;
-        return;
-    }
-    else{
-        cout << "\nNo one is the winner\n";
-    }
-}
-
-int main(){
-    block* head=NULL;
-    string n;
-    genesis_block(&head);
-
-    int exit=5 ,temp;
-    while(exit!=0){
-        if(!verify(head)){
-            cout<<"blockchain is compromised\n";
-            return 0; 
+    string getLastHash() const {
+        string newHash;
+        if (head == nullptr) {
+            newHash = "f1534392279bddbf9d43dde8701cb5be14b82f76ec6607bf8d6ad557f60f304e";
+        } else {
+            Block* last = head;
+            while (last->next != nullptr) {
+                last = last->next;
+            }
+            newHash = last->hash;
         }
-        cout<<"\nChoose the candidate to vote:\n\n	1.RAHUL SINGH\n	2.KOMAL GUPTA\n	3.ABHISHEK TOMAR\n	4.Any other number to choose NOTA\n";
-        cout<<"->"<<" ";
-        getline(cin,n);
-        new_block(&head,n);
-        cout<<"\nTO CONTINUE PRESS ANY NUMBER\n\nTO EXIT PRESS '0'\n";
-        cin>>exit;
+        return newHash;
+    }
+
+    bool verify() const {
+        string lastHash = getLastHash();
+        string fileHash;
+        ifstream hashFile("lasthash.txt");
+        if (hashFile.is_open()) {
+            while (hashFile >> fileHash) {
+            }
+            hashFile.close();
+            return (fileHash == lastHash);
+        }
+        return false;
+    }
+
+    void print() const {
+        Block* current = head;
+        while (current != nullptr) {
+            cout << current->data << "->";
+            current = current->next;
+        }
+        cout << "END" << endl;
+    }
+
+    void checkWinner() const {
+        int count1 = 0, count2 = 0, count3 = 0;
+        Block* current = head;
+        while (current != nullptr) {
+            if (current->data == "1") {
+                count1++;
+            } else if (current->data == "2") {
+                count2++;
+            } else if (current->data == "3") {
+                count3++;
+            }
+            current = current->next;
+        }
+
+        vector<pair<int, string>> candidates = {
+            {count1, "RAHUL SINGH"},
+            {count2, "KOMAL GUPTA"},
+            {count3, "ABHISHEK TOMAR"}
+        };
+
+        sort(candidates.rbegin(), candidates.rend()); // Sort in descending order
+
+        if (candidates.empty() || candidates[0].first == 0) {
+            cout << "No one is the winner" << endl;
+        } else {
+            cout << "Winner is: " << candidates[0].second << endl;
+        }
+    }
+
+    void saveToFile(const string& hash) const {
+        ofstream hashFile("lasthash.txt");
+        if (hashFile.is_open()) {
+            hashFile << hash;
+            hashFile.close();
+        } else {
+            cout << "Unable to save hash to file." << endl;
+        }
+    }
+
+    ~Blockchain() {
+        Block* current = head;
+        while (current != nullptr) {
+            Block* temp = current;
+            current = current->next;
+            delete temp;
+        }
+    }
+};
+
+int main() {
+    Blockchain blockchain;
+    blockchain.addGenesisBlock();
+
+    int exit = 5, temp;
+    while (exit != 0) {
+        if (!blockchain.verify()) {
+            cout << "Blockchain is compromised" << endl;
+            return 0;
+        }
+
+        cout << "\nChoose the candidate to vote:\n\n";
+        cout << "  1. RAHUL SINGH\n";
+        cout << "  2. KOMAL GUPTA\n";
+        cout << "  3. ABHISHEK TOMAR\n";
+        cout << "  4. Any other number to choose NOTA\n";
+        cout << "-> ";
+        
+        string input;
+        getline(cin, input);
+        blockchain.addBlock(input);
+
+        cout << "\nTO CONTINUE PRESS ANY NUMBER\n\nTO EXIT PRESS '0'\n";
+        cin >> exit;
         cin.ignore();
     }
-    cout<<"THE ORDER OF THE VOTES IS : ";
-    print(head);
-    cout<<"\nPRESS 1 TO CHECK THE WINNER OR ANY NUMBER TO EXIT :	";
-    cin>>temp;
-    if(temp==1){	
-        tocheckwinner(head);	
+
+    cout << "THE ORDER OF THE VOTES IS: ";
+    blockchain.print();
+
+    cout << "\nPRESS 1 TO CHECK THE WINNER OR ANY NUMBER TO EXIT: ";
+    cin >> temp;
+    if (temp == 1) {
+        blockchain.checkWinner();
     }
-    else {
-        return 0;
-    }		
+
+    return 0;
 }
+
